5e474ad03df334d9f8e29c833e5d2e06632c465e
jemikanegara <jemikanegara@gmail.com>
2019-02-16 02:14:52 +0800

    initial commit

---
+++ app/views/home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>myTodo Task Manager</title>
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>assets/css/custom.css" rel="stylesheet" type="text/css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery/min.js"></script>
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container-fluid">
+                <a class="brand" href="#">myTodo</a>
+                <div class="nav-collapse collapse">
+                <p class="navbar-text pull-right">
+                <!-- RIGHT TOP CONTENT -->
+
+                </p>
+                <ul class="nav">
+                    <li><a href="#">Home</a></li>
+                </ul>
+                </div> <!-- /.nav-collapse -->
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span3">
+                <div class="well sidebar-nav">
+                    <div style="margin:0 0 10px 10px;">
+                    <!-- SIDEBAR CONTENT -->
+                    </div>
+                </div> <!-- / .well -->
+            </div> <!-- / span -->
+
+            <div class="span9">
+            <!-- MAIN CONTENT -->
+                <h1>Welcome to myTodo!</h1>
+                <p>myTodo us a simple and helpful application to help you manage your day to day tasks. You can add as many tasks list as you want</p>
+            </div> <!-- /span -->
+        </div> <!-- /row -->
+
+        <hr>
+
+        <footer>
+            <p>&copy; Copyright 2013</p>
+        </footer>
+    </div> <!-- /.fluid-container -->
+</body>
+</html>
